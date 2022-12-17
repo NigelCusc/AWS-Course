2efd6eb02ed955c5d3fe10b40bbb2883f6edc7c2
Nigel Cuschieri <nigelcuschieri@gmail.com>
2022-12-17 22:13:49 +0100

    Initial Commit

---
+++ aws-lambda/greetMe/index.js
@@ -0,0 +1,24 @@
+const moment = require("moment/moment");
+
+const greeting = {
+    'en': 'Hello',
+    'fr': 'Bonjour',
+    'hi': 'Namaste',
+    'es': 'Hola'
+}
+
+exports.handler = async (event) => {
+    // Context https://docs.aws.amazon.com/lambda/latest/dg/nodejs-context.html
+    const name = event.pathParameters.name;
+    const {lang, ... info} = event.queryStringParameters;
+    let message = `${greeting[lang] ? greeting[lang] : greeting['en'] } ${name}`
+    let response = {
+        message,
+        info,
+        timestamp: moment().unix()
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    };
+}
