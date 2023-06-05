389f96dc45b20affd3c49dd9434940ac34c03e24
NigelCusc <nigelcuschieri@gmail.com>
2023-06-05 08:33:02 +0200

    Notes initial

--- serverless/notes-crud-api/notes/handler.js
+++ serverless/notes-crud-api/notes/handler.js
@@ -1,38 +1,46 @@
 'use strict';
-const { create } = require('./lib')
+const { create, update, send, deleteNote, getAllNotes } = require('./lib')
 
 module.exports.createNote = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
   try {
     const data = JSON.parse(event.body)
-    const response = await create(data)
-    if (!response.success) {
-      throw Error(response.message)
-    }
-    callback(null, { statusCode: 201, body: JSON.stringify(`Note has been created successfully. (ID: ${response.id})`) })
+    await create(data)
+    callback(null, send(201, `Note has been created successfully. (ID: ${data.id})`))
   } catch (err) {
-    callback(null, { statusCode: 500, body: JSON.stringify(`Note has not been created. Exception occured: ${err}`) })
+    callback(null, send(500, `Note has not been created. Exception occured: ${err}`))
   }
 };
 
-module.exports.updateNote = async (event) => {
+module.exports.updateNote = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
   let notesId = event.pathParameters.id;
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`The note with id: ${notesId} has been updated`),
-  };
+  try {
+    const data = JSON.parse(event.body)
+    await update(notesId, data)
+    callback(null, send(200, `The note with id: ${notesId} has been updated`))
+  } catch (err) {
+    callback(null, send(500, `Note has not been updated. Exception occured: ${err}`))
+  }
 };
 
-module.exports.deleteNote = async (event) => {
+module.exports.deleteNote = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
   let notesId = event.pathParameters.id;
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`The note with id: ${notesId} has been deleted`),
-  };
+  try {
+    await deleteNote(notesId)
+    callback(null, send(200, `The note with id: ${notesId} has been deleted`))
+  } catch (err) {
+    callback(null, send(500, `Note has not been deleted. Exception occured: ${err}`))
+  }
 };
 
-module.exports.getNotes = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`All notes`),
-  };
+module.exports.getNotes = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  try {
+    const notes = await getAllNotes()
+    callback(null, send(200, notes))
+  } catch (err) {
+    callback(null, send(500, `Failed to get all notes. Exception occured: ${err}`))
+  }
 };
--- serverless/notes-crud-api/notes/lib.js
+++ serverless/notes-crud-api/notes/lib.js
@@ -1,14 +1,25 @@
-const { v4: uuidv4 } = require('uuid')
 const AWS = require("aws-sdk")
 
-const dynamodb = new AWS.DynamoDB.DocumentClient({ region: "eu-central-1" })
+const dynamodb = new AWS.DynamoDB.DocumentClient({
+    region: "eu-central-1",
+    maxRetries: 3,
+    httpOptions: {
+        timeout: 5000
+    }
+})
 
 const _params = {
-    TableName: 'notes'
+    TableName: process.env.NOTES_TABLE_NAME || 'notes'
+}
+
+const send = (statusCode, data) => {
+    return {
+        statusCode,
+        body: JSON.stringify(data)
+    }
 }
 
 const create = async (data) => {
-    // const noteId = uuidv4()
     var params = {
         ..._params,
         Item: {
@@ -19,13 +30,46 @@ const create = async (data) => {
         ConditionExpression: "attribute_not_exists(title)"
     };
     await dynamodb.put(params).promise();
-    // dynamodb.put(params, function (err, data) {
-    //     if (err) console.log(err);
-    //     else console.log(data);
-    // });
-    return data;
+}
+
+const update = async (notesId, data) => {
+    var params = {
+        ..._params,
+        Key: { notesId },
+        UpdateExpression: "set #title = :title, #body = :body",
+        ExpressionAttributeNames: {
+            '#title': 'title',
+            '#body': 'body'
+        },
+        ExpressionAttributeValues: {
+            ':title': data.title,
+            ':body': data.body
+        },
+        ConditionExpression: 'attribute_exists(notesId)'
+    };
+    await dynamodb.update(params).promise();
+}
+
+const deleteNote = async (notesId) => {
+    var params = {
+        ..._params,
+        Key: { notesId },
+        ConditionExpression: 'attribute_exists(notesId)'
+    };
+    await dynamodb.delete(params).promise();
+}
+
+const getAllNotes = async () => {
+    var params = {
+        ..._params,
+    };
+    return await dynamodb.scan(params).promise();
 }
 
 module.exports = {
-    create
+    create,
+    update,
+    send,
+    deleteNote,
+    getAllNotes
 }
